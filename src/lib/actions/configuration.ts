cf3a78e36bf135f72fa754d7700dee7f6b47d70d
Nam <namdeveloper.ca@gmail.com>
2024-05-10 11:33:37 +0700

    design

--- src/lib/actions/configuration.ts
+++ src/lib/actions/configuration.ts
@@ -1,7 +1,29 @@
+import { CaseColor, CaseFinish, CaseMaterial, PhoneModel } from '@prisma/client'
 import { db } from '../db'
 
 export const getConfiguration = async (configId: string) => {
   return db.configuration.findUnique({
     where: { id: configId },
+  })
+}
+
+export type SaveConfigType = {
+  color: CaseColor
+  finish: CaseFinish
+  material: CaseMaterial
+  model: PhoneModel
+  configId: string
+}
+
+export const saveConfigurationDb = async ({
+  color,
+  finish,
+  material,
+  model,
+  configId,
+}: SaveConfigType) => {
+  await db.configuration.update({
+    where: { id: configId },
+    data: { color, finish, material, model },
   })
 }

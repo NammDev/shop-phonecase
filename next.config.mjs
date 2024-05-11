9addb2944123ea5f762279dc39e868688e53bf98
Nam <namdeveloper.ca@gmail.com>
2024-05-11 23:08:40 +0700

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
         source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://casecobra-72a99c2db514.herokuapp.com',
-          }, // replace this your actual origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
+        headers: (req) => {
+          const origin = req.headers.origin
+          return [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: origin },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value:
+                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+            },
+          ]
+        },
       },
     ]
   },

2f5f282db27eb2c0dd009d5a7d95bf5675fecfb1
Nam <namdeveloper.ca@gmail.com>
2024-05-11 23:02:45 +0700

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,24 +4,13 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/api/:path*',
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-      {
-        // matching heroku
-        source: 'https://casecobra-72a99c2db514.herokuapp.com',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://casecobra-72a99c2db514.herokuapp.com',
+          }, // replace this your actual origin
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',

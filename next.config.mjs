747d7edb546f3275d8000ce1f6eedf93b08fabca
Nam <namdeveloper.ca@gmail.com>
2024-05-11 23:15:59 +0700

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: (req) => {
-          const origin = req.headers.origin
-          return [
-            { key: 'Access-Control-Allow-Credentials', value: 'true' },
-            { key: 'Access-Control-Allow-Origin', value: origin },
-            { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-            {
-              key: 'Access-Control-Allow-Headers',
-              value:
-                'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-            },
-          ]
-        },
-      },
-    ]
-  },
   images: {
     domains: ['utfs.io'],
   },

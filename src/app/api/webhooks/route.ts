63962bc8fccd133eb9d33930ea6b0a2d0890838b
Nam <namdeveloper.ca@gmail.com>
2024-05-11 20:52:30 +0700

    webhook

---
+++ src/app/api/webhooks/route.ts
@@ -0,0 +1,60 @@
+import { updateOrderStripe } from '@/lib/actions/order'
+import { stripe } from '@/lib/stripe'
+import { headers } from 'next/headers'
+import { NextRequest, NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+export async function POST(req: NextRequest) {
+  try {
+    // make sure request from stripe
+    const signature = headers().get('stripes-signature')
+    if (!signature) return new Response('Invalid signature', { status: 400 })
+
+    // get event from stripe
+    const body = await req.text()
+    const event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+
+    // user successful paid
+    if (event.type === 'checkout.session.completed') {
+      if (!event.data.object.customer_details?.email) throw new Error('Missing user email')
+
+      // Get metadata from client send to stripe
+      const session = event.data.object as Stripe.Checkout.Session
+      const { userId, orderId } = session.metadata || {
+        userId: null,
+        orderId: null,
+      }
+      if (!userId || !orderId) throw new Error('Invalid request metadata')
+
+      // update order
+      const updatedOrder = await updateOrderStripe(orderId, session)
+
+      //   await resend.emails.send({
+      //     from: 'CaseCobra <hello@joshtriedcoding.com>',
+      //     to: [event.data.object.customer_details.email],
+      //     subject: 'Thanks for your order!',
+      //     react: OrderReceivedEmail({
+      //       orderId,
+      //       orderDate: updatedOrder.createdAt.toLocaleDateString(),
+      //       // @ts-ignore
+      //       shippingAddress: {
+      //         name: session.customer_details!.name!,
+      //         city: session.shipping_details!.address!.city!,
+      //         country: session.shipping_details!.address!.country!,
+      //         postalCode: session.shipping_details!.address!.postal_code!,
+      //         street: session.shipping_details!.address!.line1!,
+      //         state: session.shipping_details!.address!.state,
+      //       },
+      //     }),
+      //   })
+      return NextResponse.json({ result: event, ok: true })
+    }
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ message: 'Something went wrong', ok: false }, { status: 500 })
+  }
+}
--- src/lib/actions/order.ts
+++ src/lib/actions/order.ts
@@ -1,4 +1,5 @@
 'use server'
+import Stripe from 'stripe'
 import { db } from '../db'
 
 export const getOrder = async (userId: string, configId: string) => {
@@ -16,6 +17,39 @@ export const createOrder = async (userId: string, configId: string, price: numbe
       userId: userId,
       configurationId: configId,
       amount: price / 100,
+    },
+  })
+}
+
+export const updateOrderStripe = async (orderId: string, session: Stripe.Checkout.Session) => {
+  const billingAddress = session.customer_details!.address
+  const shippingAddress = session.shipping_details!.address
+  return await db.order.update({
+    where: {
+      id: orderId,
+    },
+    data: {
+      isPaid: true,
+      shippingAddress: {
+        create: {
+          name: session.customer_details!.name!,
+          city: shippingAddress!.city!,
+          country: shippingAddress!.country!,
+          postalCode: shippingAddress!.postal_code!,
+          street: shippingAddress!.line1!,
+          state: shippingAddress!.state,
+        },
+      },
+      billingAddress: {
+        create: {
+          name: session.customer_details!.name!,
+          city: billingAddress!.city!,
+          country: billingAddress!.country!,
+          postalCode: billingAddress!.postal_code!,
+          street: billingAddress!.line1!,
+          state: billingAddress!.state,
+        },
+      },
     },
   })
 }

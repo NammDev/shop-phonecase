b83b4628c5e897ecd7fcda5d4e2937a9dca84095
Nam <namdeveloper.ca@gmail.com>
2024-05-12 10:09:10 +0700

    handle cors

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
           {
             key: 'Access-Control-Allow-Origin',
             value: 'https://casecobra-72a99c2db514.herokuapp.com',
-          }, // replace this your actual origin
+          },
           { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',

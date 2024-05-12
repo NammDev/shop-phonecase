3e5a910b021ae054e9ede4a24c22f6a1eb0ba0ba
Nam <namdeveloper.ca@gmail.com>
2024-05-12 11:29:34 +0700

    thank you page

--- src/lib/actions/order.ts
+++ src/lib/actions/order.ts
@@ -1,6 +1,8 @@
 'use server'
+
 import Stripe from 'stripe'
 import { db } from '../db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export const getOrder = async (userId: string, configId: string) => {
   return await db.order.findFirst({
@@ -52,4 +54,31 @@ export const updateOrderStripe = async (orderId: string, session: Stripe.Checkou
       },
     },
   })
+}
+
+export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  if (!user?.id || !user.email) {
+    throw new Error('You need to be logged in to view this page.')
+  }
+
+  const order = await db.order.findFirst({
+    where: { id: orderId, userId: user.id },
+    include: {
+      billingAddress: true,
+      configuration: true,
+      shippingAddress: true,
+      user: true,
+    },
+  })
+
+  if (!order) throw new Error('This order does not exist.')
+
+  if (order.isPaid) {
+    return order
+  } else {
+    return false
+  }
 }

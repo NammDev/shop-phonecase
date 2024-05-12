ea8a5ec496b925d61b57a79dd95778cb62ccfbda
Nam <namdeveloper.ca@gmail.com>
2024-05-12 11:41:19 +0700

    thank you page

--- src/lib/actions/order.ts
+++ src/lib/actions/order.ts
@@ -3,6 +3,7 @@
 import Stripe from 'stripe'
 import { db } from '../db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { OrderStatus } from '@prisma/client'
 
 export const getOrder = async (userId: string, configId: string) => {
   return await db.order.findFirst({
@@ -81,4 +82,62 @@ export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
   } else {
     return false
   }
+}
+
+export const changeOrderStatus = async ({
+  id,
+  newStatus,
+}: {
+  id: string
+  newStatus: OrderStatus
+}) => {
+  await db.order.update({
+    where: { id },
+    data: { status: newStatus },
+  })
+}
+
+export const getOrders = async () => {
+  return await db.order.findMany({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      user: true,
+      shippingAddress: true,
+    },
+  })
+}
+
+export const getOrdersAggregate = async () => {
+  const lastWeekSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 7)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  })
+
+  const lastMonthSum = await db.order.aggregate({
+    where: {
+      isPaid: true,
+      createdAt: {
+        gte: new Date(new Date().setDate(new Date().getDate() - 30)),
+      },
+    },
+    _sum: {
+      amount: true,
+    },
+  })
+  return { lastWeekSum, lastMonthSum }
 }
